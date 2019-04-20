fbcddf12d95fb29e13c9490d0614382e19e66760
urbanze <jvitor-2005@hotmail.com>
2019-04-20 16:49:58 -0300

    HTTP Example

---
+++ examples/ota_http/http_default.ino
@@ -0,0 +1,18 @@
+#include <http.h>
+#include <tcp.h>
+#include <WiFi.h>
+
+
+void setup()
+{
+  WiFi.mode(WIFI_STA);//WiFi in STATION mode
+  WiFi.begin("B", "");//Connect in your wifi
+
+  OTA_HTTP ota;//Create OTA_HTTP objetct
+  ota.init();//Initialize OTA webpage in your IP and PORT 8080
+}
+
+void loop()
+{
+
+}
