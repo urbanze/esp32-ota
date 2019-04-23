a3c9c7fe68622b5e9b2c6637b8592eaa6f44e713
José Morais <jvitor-2005@hotmail.com>
2019-04-23 17:03:28 -0300

    Update http_default.ino

--- examples/ota_http/http_default.ino
+++ examples/ota_http/http_default.ino
@@ -8,7 +8,7 @@ void setup()
   WiFi.mode(WIFI_STA);//WiFi in STATION mode
   WiFi.begin("", "");//Connect in your wifi
 
-  OTA_HTTP ota;//Create OTA_HTTP objetct
+  OTA_HTTP ota;//Create OTA_HTTP object
   ota.init();//Initialize OTA webpage in your IP and PORT 8080 (like: 192.168.x.x:8080)
 }
 
