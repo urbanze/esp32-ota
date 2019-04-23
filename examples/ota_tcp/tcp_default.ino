190e7cf06f8ae9d1f316781ce98624fff651cec3
José Morais <jvitor-2005@hotmail.com>
2019-04-23 17:03:43 -0300

    Update tcp_default.ino

--- examples/ota_tcp/tcp_default.ino
+++ examples/ota_tcp/tcp_default.ino
@@ -8,7 +8,7 @@ void setup()
   WiFi.mode(WIFI_STA);//WiFi in STATION mode
   WiFi.begin("", "");//Connect in your wifi
 
-  OTA_TCP ota;//Create OTA_TCP objetct
+  OTA_TCP ota;//Create OTA_TCP object
   ota.init();//Initialize OTA TCP in your IP and port 22180 (see Raspberry Pi sending update on tcp_img1)
 }
 
